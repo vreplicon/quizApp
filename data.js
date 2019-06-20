f3ec3939c0caf178dee9680b5418ccf0e081b100
Violet Replicon <vreplicon@gmail.com>
2019-06-19 19:44:46 -0700

    Added questions and answers to a seperate data file

---
+++ data.js
@@ -0,0 +1,94 @@
+const QNA = [
+    {
+        q:"What type of celestial body is the sun?",
+        a1:"A planet",
+        a2:"A star",
+        a3:"A rock",
+        a4:"A galaxy",
+        ans:2,
+        note: "The sun is the closest star to our planet. The next\
+         closest star is Alpha Centari at 4.4 light years"
+    },
+    {
+        q:"What is the most common element in the universe?",
+        a1:"Oxygen",
+        a2:"Helium",
+        a3:"Hydrogen",
+        a4:"Carbon",
+        ans:3,
+        note: "Hydrogen is the most common elemet in the universe\
+         with helium being the second most common."
+    },
+    {
+        q:"What is the largest planet in our solar system?",
+        a1:"Earth",
+        a2:"Saturn",
+        a3:"Jupiter",
+        a4:"Neptune",
+        ans:3,
+        note: "Jupiter is the largest planet with a radius of something"
+    },
+    {
+        q:"Which of these is a dwarf planet?",
+        a1:"Mercury",
+        a2:"Venus",
+        a3:"Pluto",
+        a4:"Uranus",
+        ans:3,
+        note: "Pluto was declared an dwarf planet in YEAR"
+    },
+    {
+        q:"Which of these is not a moon of Jupiter?",
+        a1:"Io",
+        a2:"Europa",
+        a3:"Castillo",
+        a4:"Titan",
+        ans:4,
+        note: "Titan is the largest moon of Saturn."
+    },
+    {
+        q:"How many satellites have left our solar system?",
+        a1:"None",
+        a2:"1",
+        a3:"2",
+        a4:"3",
+        ans:2,
+        note: "SOMETHIGN"
+    },
+    {
+        q:"What was the original name of Uranus",
+        a1:"The Georgium Sidus",
+        a2:"Hyacintho Pila Magna",
+        a3:"Ares",
+        a4:"Planet X",
+        ans:1,
+        note: "JDKSLJGKL"
+    },
+    {
+        q:"The universe is:",
+        a1:"Contracting",
+        a2:"Expanding",
+        a3:"Rotating",
+        a4:"Stable",
+        ans:2,
+        note: "JKLDSJFKLDJS"
+    },
+    {
+        q:"What planet is named after the god of war?",
+        a1:"Venus",
+        a2:"Neptune",
+        a3:"Mars",
+        a4:"Mercury",
+        ans:3,
+        note: "JKDSHGJKD"
+    },
+    {
+        q:"How many gaseous planets are in our solar system?",
+        a1:"2",
+        a2:"4",
+        a3:"6",
+        a4:"8",
+        ans:2,
+        note: "JKDFSJKGLSJK"
+    }
+];
